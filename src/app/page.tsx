de7cf84f887dc0df5e88c27ddf390754218ed347
BriantLuu <luutriminh18@gmail.com>
2023-03-12 16:21:25 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,6 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{' '}
-            BriantLuu
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
